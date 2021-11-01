90d56fbc7c4cc1061904fb1ffbc04bb5551757c1
TrishaS1401 <56110148+TrishaS1401@users.noreply.github.com>
2021-11-01 15:16:11 +0530

    Update pracs.py

--- pracs.py
+++ pracs.py
@@ -1,4 +1,4 @@
-import os
+'''import os
 
 def sum(x, n):
     total = 1.0
@@ -10,6 +10,16 @@ def sum(x, n):
 if __name__== '__main__':
     x = int(os.getenv("x"))
     n = int(os.getenv("n"))
-    print ("Sum is: {0:.4f}".format(sum(x, n)))
+    print ("Sum is: {0:.4f}".format(sum(x, n)))'''
+
+import os
+n = int(os.getenv("n")) 
+x = int(os.getenv("x")) 
+
+sum1 = 1
+
+for i in range(2,n+1):
+    sum1 = sum1 + (((x**i)-i)/i)
+print("The sum of series is ",round(sum1,2))
     
 
