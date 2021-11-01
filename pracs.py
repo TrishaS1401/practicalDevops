62d2e3580686204973d15f31db13087d11795eef
TrishaS1401 <56110148+TrishaS1401@users.noreply.github.com>
2021-11-01 13:56:14 +0530

    Add files via upload

---
+++ pracs.py
@@ -0,0 +1,15 @@
+import os
+
+def sum(x, n):
+    total = 1.0
+    for i in range (2, n + 1):
+        total = total + ((pow(x, i) - i) / (i))
+    return total
+
+
+if __name__== '__main__':
+    x = int(os.getenv("x"))
+    n = int(os.getenv("n"))
+    print ("Sum is: {0:.4f}".format(sum(x, n)))
+    
+
